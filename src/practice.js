00f5ba2fbb8a8867c2cd17464bf260f5db5c58be
Bowie <bowie@thinkful.com>
2020-01-20 16:57:04 -0800

    init commit

---
+++ src/practice.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const knex = require('knex')
+
+const knexInstance = knex({
+    client: 'pg',
+    connection: 'process.env.DB_URL'
+})
+
+knexInstance.from('amazong_products').select('*')
+   .then(result => {
+     console.log(result)
+    })
